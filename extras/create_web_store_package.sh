f7f793245acbec32aefdc1aba859abef9880355a
Gordon Williams <gw@pur3.co.uk>
2013-09-19 15:50:20 +0100

    published 1.26

--- extras/create_web_store_package.sh
+++ extras/create_web_store_package.sh
@@ -2,4 +2,5 @@
 cd `dirname $0`
 cd ..
 rm -f EspruinoSerialTerminal.zip
+find . -name "*~" -delete
 zip EspruinoSerialTerminal.zip main.html manifest.json css/*.css css/ui-lightness/*.css css/ui-lightness/images/* img/*.png js/*.js blockly/* blockly/media/*
